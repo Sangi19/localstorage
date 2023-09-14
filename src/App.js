db7623f59d78c25e7c71df568a7edab764fa04c9
sangeethaRamkumar <sangiammu1020@gmail.com>
2023-09-14 15:18:35 +0530

    local storage session storage differences

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import "./styles/bootstrap.min.css"
 function App() {
+  localStorage.setItem("name", "sam")
+  sessionStorage.setItem("id","123")
   return (
     <>
-      <h1>hello</h1>
+      <h2>Welcome to local Storage page {localStorage.getItem("name")}</h2>
+      <h3>session storage id is {sessionStorage.getItem("id")}</h3>
     </>
   );
 }
