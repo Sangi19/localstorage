ff22b68c4d6d22b060ed5ed4016983e8d0b7dc76
sangeethaRamkumar <sangiammu1020@gmail.com>
2023-09-15 14:10:50 +0530

    add components for login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 function App() {
-  localStorage.setItem("name", "sam")
-  sessionStorage.setItem("id","123")
+  // localStorage.setItem("name", "sam")
+  // sessionStorage.setItem("id","123")
   return (
     <>
-      <h2>Welcome to local Storage page {localStorage.getItem("name")}</h2>
-      <h3>session storage id is {sessionStorage.getItem("id")}</h3>
+    <h1>Home</h1>
+      {/* <h2>Welcome to local Storage page {localStorage.getItem("name")}</h2>
+      <h3>session storage id is {sessionStorage.getItem("id")}</h3> */}
     </>
   );
 }
